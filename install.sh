1d414814701e87f061b4e0425ceecc53f1f99494
Sébastien Allamand <sallaman@amazon.com>
2024-08-07 17:15:43 +0000

    remove tty that break on codebuild
    Signed-off-by: Sébastien Allamand <sallaman@amazon.com>

--- install.sh
+++ install.sh
@@ -10,9 +10,15 @@ ROOTDIR=$SCRIPTDIR
 # export TF_VAR_ssh_key_basepath="/home/ec2-user/.ssh"
 
 # Deploy the infrastructure
+echo "Deploy Git"
 ${ROOTDIR}/terraform/codecommit/deploy.sh
+echo "Configure Git"
 source ${ROOTDIR}/setup-git.sh
+echo "Deploy Hub Cluster"
 ${ROOTDIR}/terraform/hub/deploy.sh
+echo "Deploy Spoke Staging"
 ${ROOTDIR}/terraform/spokes/deploy.sh staging
+echo "Deploy Spoke Prod"
 ${ROOTDIR}/terraform/spokes/deploy.sh prod
+echo "Configure Kubectl"
 source ${ROOTDIR}/setup-kubectx.sh
