f469f4a2c2dfec215f0a5177f131b68228f9a168
Sébastien Allamand <sallaman@amazon.com>
2024-08-11 14:54:29 +0000

    add aws-delete-vpc
    Signed-off-by: Sébastien Allamand <sallaman@amazon.com>

--- terraform/hub/destroy.sh
+++ terraform/hub/destroy.sh
@@ -22,25 +22,28 @@ terraform -chdir=$SCRIPTDIR destroy -target="module.eks_blueprints_addons" -auto
 terraform -chdir=$SCRIPTDIR destroy -target="module.eks" -auto-approve
 
 echo "remove VPC endpoints"
-VPCID=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=fleet-hub-cluster" --query "Vpcs[*].VpcId" --output text)
-
-echo "Checking if VPC endpoints exist..."
-vpc_endpoint_names=("com.amazonaws.eu-west-1.guardduty-data" "com.amazonaws.eu-west-1.ssm" "com.amazonaws.eu-west-1.ec2messages" "com.amazonaws.eu-west-1.ssmmessages" "com.amazonaws.eu-west-1.s3")
-for endpoint_name in "${vpc_endpoint_names[@]}"; do
-    endpoint_exists=$(aws ec2 describe-vpc-endpoints --filters "Name=service-name,Values=$endpoint_name" "Name=vpc-id,Values=$VPCID" --query "VpcEndpoints[*].VpcEndpointId" --output text 2>/dev/null)
-
-    if [ -n "$endpoint_exists" ]; then
-        echo "Deleting VPC endpoint $vpc_endpoint_id..."
-        aws ec2 delete-vpc-endpoints --vpc-endpoint-ids "$vpc_endpoint_id"
-    fi
-done
-
+VPCID=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=fleet-spoke-${env}" --query "Vpcs[*].VpcId" --output text)
 if [ -n "$VPCID" ]; then
     echo "VPC ID: $VPCID"
+
+    echo "Cleaning VPC endpoints if exists..."
+    vpc_endpoint_names=("com.amazonaws.eu-west-1.guardduty-data" "com.amazonaws.eu-west-1.ssm" "com.amazonaws.eu-west-1.ec2messages" "com.amazonaws.eu-west-1.ssmmessages" "com.amazonaws.eu-west-1.s3")
+    for endpoint_name in "${vpc_endpoint_names[@]}"; do
+        endpoint_exists=$(aws ec2 describe-vpc-endpoints --filters "Name=service-name,Values=$endpoint_name" "Name=vpc-id,Values=$VPCID" --query "VpcEndpoints[*].VpcEndpointId" --output text 2>/dev/null)
+
+        if [ -n "$endpoint_exists" ]; then
+            echo "Deleting VPC endpoint $vpc_endpoint_id..."
+            aws ec2 delete-vpc-endpoints --vpc-endpoint-ids "$vpc_endpoint_id"
+        fi
+    done
+
+
+    echo "Cleaning VPC $VPCID"
     aws-delete-vpc -vpc-id=$VPCID
+
 else
-    echo "VPC with tag Name=fleet-hub-cluster not found"
-fi    
+    echo "VPC with tag Name=fleet-spoke-${env} not found"
+fi   
 
 terraform -chdir=$SCRIPTDIR destroy -target="module.vpc" -auto-approve
 terraform -chdir=$SCRIPTDIR destroy -auto-approve
