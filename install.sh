d8e10419d58a6a9c2b41ca41088c8eba483a08b2
Sébastien Allamand <sallaman@amazon.com>
2024-08-12 15:52:54 +0000

    temporarilly deactivate cluster creation
    Signed-off-by: Sébastien Allamand <sallaman@amazon.com>

--- install.sh
+++ install.sh
@@ -15,11 +15,11 @@ mkdir -p ~/.ssh
 ${ROOTDIR}/terraform/codecommit/deploy.sh
 echo "Configure Git"
 source ${ROOTDIR}/setup-git.sh
-echo "Deploy Hub Cluster"
-${ROOTDIR}/terraform/hub/deploy.sh
-echo "Deploy Spoke Staging"
-${ROOTDIR}/terraform/spokes/deploy.sh staging
-echo "Deploy Spoke Prod"
-${ROOTDIR}/terraform/spokes/deploy.sh prod
-echo "Configure Kubectl"
-source ${ROOTDIR}/setup-kubectx.sh
+# echo "Deploy Hub Cluster"
+# ${ROOTDIR}/terraform/hub/deploy.sh
+# echo "Deploy Spoke Staging"
+# ${ROOTDIR}/terraform/spokes/deploy.sh staging
+# echo "Deploy Spoke Prod"
+# ${ROOTDIR}/terraform/spokes/deploy.sh prod
+# echo "Configure Kubectl"
+# source ${ROOTDIR}/setup-kubectx.sh
