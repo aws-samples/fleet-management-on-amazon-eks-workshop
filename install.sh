575f62a092c365ae07dc667b58fe76b23b846b4d
Sébastien Allamand <sallaman@amazon.com>
2024-08-14 08:24:51 +0000

    avoid error argocd permission error
    Signed-off-by: Sébastien Allamand <sallaman@amazon.com>

--- install.sh
+++ install.sh
@@ -16,8 +16,30 @@ ${ROOTDIR}/terraform/codecommit/deploy.sh
 echo "Configure Git"
 source ${ROOTDIR}/setup-git.sh
 
-echo "youyou - check identity"
-aws sts get-caller-identity
+# echo "youyou - check identity"
+# aws sts get-caller-identity
+# #arn:aws:sts::912939173604:assumed-role/workshop-team-stack-IDEFleet7IdeRoleC9AC90CF-xixImgYBYbAX/i-0d148cf3c85d58216
+# # Get the IAM role of the instance
+# INSTANCE_ROLE=$(aws sts get-caller-identity | jq ".Arn" -r | cut -d'/' -f1,2)
+# # Check if the IAM role matches the expected pattern
+# if [[ $INSTANCE_ROLE =~ ^arn:aws:iam::[0-9]+:role/workshop-team-stack-IDEFleet.* ]]; then
+#     echo "IAM role matches the expected pattern: $INSTANCE_ROLE"
+
+#     # List of EKS clusters to check
+#     clusters=("fleet-hub-cluster" "fleet-spoke-prod" "fleet-spoke-staging")
+
+#     # Check if EKS clusters exist and add the IAM role to the cluster access list
+#     for cluster in "${clusters[@]}"; do
+#         if aws eks describe-cluster --name "$cluster" --query "cluster.status" --output text 2>/dev/null; then
+#             echo "EKS cluster '$cluster' exists. Adding IAM role to the access list..."
+#             aws eks associate-iam-role-to-cluster --cluster-name "$cluster" --role-arn "$INSTANCE_ROLE" --role-name AmazonEKSClusterAdminPolicy
+#         else
+#             echo "EKS cluster '$cluster' does not exist."
+#         fi
+#     done
+# else
+#     echo "IAM role does not match the expected pattern: $INSTANCE_ROLE"
+# fi
 
 echo "Deploy Hub Cluster"
 ${ROOTDIR}/terraform/hub/deploy.sh
