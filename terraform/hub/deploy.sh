01b4fffb310682afe2c6c878a378b0f715ac2616
Sébastien Allamand <sallaman@amazon.com>
2024-08-14 15:43:25 +0000

    debug only aws command
    Signed-off-by: Sébastien Allamand <sallaman@amazon.com>

--- terraform/hub/deploy.sh
+++ terraform/hub/deploy.sh
@@ -10,7 +10,7 @@ ROOTDIR="$(cd ${SCRIPTDIR}/../..; pwd )"
 terraform -chdir=$SCRIPTDIR init --upgrade
 
 echo "Deploy Hub cluster"
-terraform -chdir=$SCRIPTDIR apply -target="module.eks" -auto-approve
-terraform -chdir=$SCRIPTDIR apply -target="module.eks_blueprints_addons" -auto-approve
-terraform -chdir=$SCRIPTDIR apply -target="module.gitops_bridge_bootstrap" -auto-approve
+#terraform -chdir=$SCRIPTDIR apply -target="module.eks" -auto-approve
+#terraform -chdir=$SCRIPTDIR apply -target="module.eks_blueprints_addons" -auto-approve
+#terraform -chdir=$SCRIPTDIR apply -target="module.gitops_bridge_bootstrap" -auto-approve
 terraform -chdir=$SCRIPTDIR apply -auto-approve
