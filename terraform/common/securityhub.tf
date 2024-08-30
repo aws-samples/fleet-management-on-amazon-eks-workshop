510fd4fc7b9868bda5ac1159e34d82da4e7b642a
Sébastien Allamand <sallaman@amazon.com>
2024-08-30 12:54:06 +0000

    add securityhub activation
    Signed-off-by: Sébastien Allamand <sallaman@amazon.com>

--- terraform/common/securityhub.tf
+++ terraform/common/securityhub.tf
@@ -1,12 +1,4 @@
 resource "aws_securityhub_account" "example" {}
-# resource "aws_securityhub_account" "example" {
-#   depends_on = [aws_securityhub_standards_subscription.example]
-# }
-
-# resource "aws_securityhub_standards_subscription" "example" {
-#   depends_on    = [aws_securityhub_account.example]
-#   standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
-# }
 
 resource "aws_securityhub_insight" "kyverno_disallow_privileged" {
   group_by_attribute = "ProductName"
