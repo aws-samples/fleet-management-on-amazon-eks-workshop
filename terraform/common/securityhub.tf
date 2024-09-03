51a56ee86330b8882e496ae51784ef82c2525a46
Sébastien Allamand <sebastien@allamand.com>
2024-09-03 16:22:01 +0200

    Kyverno app (#23)
    * add Kyverno
    
    Signed-off-by: Sebastien Allamand <sallaman@amazon.com>
    
    * remove psa namespace policy
    
    * fix kyverno insights in apps (except 3 for the workshop)
    
    ---------
    
    Signed-off-by: Sebastien Allamand <sallaman@amazon.com>

--- terraform/common/securityhub.tf
+++ terraform/common/securityhub.tf
@@ -1,4 +1,4 @@
-#resource "aws_securityhub_account" "example" {}
+
 
 resource "aws_securityhub_insight" "kyverno_disallow_privileged" {
   group_by_attribute = "ProductName"
@@ -82,5 +82,4 @@ resource "aws_securityhub_insight" "kyverno_require-run-as-nonroot" {
       value      = "NOTIFIED"
     }
   }
-
 }
