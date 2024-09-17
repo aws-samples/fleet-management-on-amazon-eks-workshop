5a6ac7cf364a1e30f6c08b01eb7b7cf839ba6897
Sébastien Allamand <sallaman@amazon.com>
2024-09-17 11:51:20 +0000

    update kyverno answers
    Signed-off-by: Sébastien Allamand <sallaman@amazon.com>

--- hack/.bashrc.d/answers.bash
+++ hack/.bashrc.d/answers.bash
@@ -39,16 +39,45 @@ function app_prod_staging (){
   wait-for-lb $(kubectl --context fleet-prod-cluster get svc -n ui ui-nlb -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
 }
 
-function apps_fix_kyverno_insights (){
+function apps_fix_kyverno_insights(){
+  apps_fix_kyverno_insights_staging
+  apps_fix_kyverno_insights_prod
+}
+
+function apps_fix_kyverno_insights_staging (){
   # Fix Kyverno Insights
 
-  cp $WORKSHOP_DIR/gitops/solutions/module-kyverno/apps/backend/catalog/base/deployment.yaml $GITOPS_DIR/apps/backend/catalog/base/deployment.yaml
-  cp $WORKSHOP_DIR/gitops/solutions/module-kyverno/apps/frontend/assets/base/deployment.yaml $GITOPS_DIR/apps/frontend/assets/base/deployment.yaml
-  cp $WORKSHOP_DIR/gitops/solutions/module-kyverno/apps/frontend/ui/base/deployment.yaml $GITOPS_DIR/apps/frontend/ui/base/deployment.yaml
+  #staging
+  cp $WORKSHOP_DIR/gitops/solutions/module-kyverno/apps/backend/catalog/staging/kustomization.yaml $GITOPS_DIR/apps/backend/catalog/staging/kustomization.yaml
+  cp $WORKSHOP_DIR/gitops/solutions/module-kyverno/apps/backend/catalog/staging/deployment.yaml $GITOPS_DIR/apps/backend/catalog/staging/deployment.yaml
+  cp $WORKSHOP_DIR/gitops/solutions/module-kyverno/apps/frontend/assets/staging/kustomization.yaml $GITOPS_DIR/apps/frontend/assets/staging/kustomization.yaml
+  cp $WORKSHOP_DIR/gitops/solutions/module-kyverno/apps/frontend/assets/staging/deployment.yaml $GITOPS_DIR/apps/frontend/assets/staging/deployment.yaml
+  cp $WORKSHOP_DIR/gitops/solutions/module-kyverno/apps/frontend/ui/staging/kustomization.yaml $GITOPS_DIR/apps/frontend/ui/staging/kustomization.yaml
+  cp $WORKSHOP_DIR/gitops/solutions/module-kyverno/apps/frontend/ui/staging/deployment.yaml $GITOPS_DIR/apps/frontend/ui/staging/deployment.yaml
+
   git -C $GITOPS_DIR/apps status
   git -C $GITOPS_DIR/apps diff | cat
   git -C $GITOPS_DIR/apps add .
-  git -C $GITOPS_DIR/apps commit -m "Fix Kyverno in catalog"
+  git -C $GITOPS_DIR/apps commit -m "Fix Kyverno in catalog for Staging"
+  git -C $GITOPS_DIR/apps push
+
+}
+
+function apps_fix_kyverno_insights_staging (){
+  # Fix Kyverno Insights
+
+  #staging
+  cp $WORKSHOP_DIR/gitops/solutions/module-kyverno/apps/backend/catalog/prod/kustomization.yaml $GITOPS_DIR/apps/backend/catalog/prod/kustomization.yaml
+  cp $WORKSHOP_DIR/gitops/solutions/module-kyverno/apps/backend/catalog/prod/deployment.yaml $GITOPS_DIR/apps/backend/catalog/prod/deployment.yaml
+  cp $WORKSHOP_DIR/gitops/solutions/module-kyverno/apps/frontend/assets/prod/kustomization.yaml $GITOPS_DIR/apps/frontend/assets/prod/kustomization.yaml
+  cp $WORKSHOP_DIR/gitops/solutions/module-kyverno/apps/frontend/assets/prod/deployment.yaml $GITOPS_DIR/apps/frontend/assets/prod/deployment.yaml
+  cp $WORKSHOP_DIR/gitops/solutions/module-kyverno/apps/frontend/ui/prod/kustomization.yaml $GITOPS_DIR/apps/frontend/ui/prod/kustomization.yaml
+  cp $WORKSHOP_DIR/gitops/solutions/module-kyverno/apps/frontend/ui/prod/deployment.yaml $GITOPS_DIR/apps/frontend/ui/prod/deployment.yaml
+
+  git -C $GITOPS_DIR/apps status
+  git -C $GITOPS_DIR/apps diff | cat
+  git -C $GITOPS_DIR/apps add .
+  git -C $GITOPS_DIR/apps commit -m "Fix Kyverno in catalog for Prod"
   git -C $GITOPS_DIR/apps push
 
 }
@@ -64,7 +93,7 @@ function apps_default_kyverno_insights (){
   git -C $GITOPS_DIR/apps status
   git -C $GITOPS_DIR/apps diff | cat
   git -C $GITOPS_DIR/apps add .
-  git -C $GITOPS_DIR/apps commit -m "Fix Kyverno in catalog"
+  git -C $GITOPS_DIR/apps commit -m "Revert Fix Kyverno in catalog"
   git -C $GITOPS_DIR/apps push
 
 }
--- hack/.bashrc.d/argocd.bash
+++ hack/.bashrc.d/argocd.bash
@@ -1,15 +1,18 @@
-function argocd_hub_credentials (){
+function argocd_credentials (){
+	export ARGOCD_SERVER=$(kubectl get svc -n argocd argocd-server -o jsonpath='{.status.loadBalancer.ingress[0].hostname}' --context $1)
+	export ARGOCD_PWD=$(kubectl get secrets argocd-initial-admin-secret -n argocd --template='{{index .data.password | base64decode}}' --context $1)
 	echo "ArgoCD Username: admin"
-	echo "ArgoCD Password: $(kubectl get secrets argocd-initial-admin-secret -n argocd --template='{{index .data.password | base64decode}}' --context fleet-hub-cluster)"
-	echo "ArgoCD URL: https://$(kubectl get svc -n argocd argocd-server -o jsonpath='{.status.loadBalancer.ingress[0].hostname}' --context fleet-hub-cluster)"
+	echo "ArgoCD Password: $ARGOCD_PWD"
+	echo "ArgoCD URL: https://$ARGOCD_SERVER"
+	argocd login $ARGOCD_SERVER --username admin --password $ARGOCD_PWD --insecure --name $1
+}
+
+function argocd_hub_credentials (){
+	argocd_credentials fleet-hub-cluster
 }
 function argocd_staging_credentials (){
-	echo "ArgoCD Username: admin"
-	echo "ArgoCD Password: $(kubectl get secrets argocd-initial-admin-secret -n argocd --template='{{index .data.password | base64decode}}' --context fleet-staging-cluster)"
-	echo "ArgoCD URL: https://$(kubectl get svc -n argocd argocd-server -o jsonpath='{.status.loadBalancer.ingress[0].hostname}' --context fleet-staging-cluster)"
+	argocd_credentials fleet-staging-cluster
 }
 function argocd_prod_credentials (){
-	echo "ArgoCD Username: admin"
-	echo "ArgoCD Password: $(kubectl get secrets argocd-initial-admin-secret -n argocd --template='{{index .data.password | base64decode}}' --context fleet-prod-cluster)"
-	echo "ArgoCD URL: https://$(kubectl get svc -n argocd argocd-server -o jsonpath='{.status.loadBalancer.ingress[0].hostname}' --context fleet-prod-cluster)"
+	argocd_credentials fleet-prod-cluster
 }
