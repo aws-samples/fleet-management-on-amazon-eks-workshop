7c400cd7004372794b81c1de998e5a28af3bb0ff
Carlos Santana <carrlos@amazon.com>
2024-09-23 17:55:23 -0400

    fix github token optional
    Signed-off-by: Carlos Santana <carrlos@amazon.com>

--- terraform/common/github.tf
+++ terraform/common/github.tf
@@ -1,4 +1,12 @@
-# create github repos for local.gitops_repos
+# To use github repos need to set the following variables
+# export TF_VAR_create_github_repos=true
+# export TF_VAR_gitea_external_url=https://github.com
+# export TF_VAR_gitea_repo_prefix="$GITHUB_USERNAME_OR_ORG/"
+# export TF_VAR_gitea_user=$GITHUB_USERNAME
+# export TF_VAR_gitea_password=$GITHUB_TOKEN
+
+
+
 resource "github_repository" "gitops_repo" {
   # do not create if var.create_github_repos is false
   for_each = { for k, v in local.gitops_repos : k => v if var.create_github_repos }
@@ -9,5 +17,5 @@ resource "github_repository" "gitops_repo" {
 }
 
 provider "github" {
-  token = var.gitea_password
+  token = var.create_github_repos ? var.gitea_password : null
 }
