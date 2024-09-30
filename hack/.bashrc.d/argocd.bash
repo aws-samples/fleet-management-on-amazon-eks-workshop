d1351a34b01a7ef415ed4b69181f2e1c3d7c108e
Carlos Santana <carrlos@amazon.com>
2024-09-30 12:34:28 -0400

    add anonymous
    Signed-off-by: Carlos Santana <carrlos@amazon.com>

--- hack/.bashrc.d/argocd.bash
+++ hack/.bashrc.d/argocd.bash
@@ -1,13 +1,19 @@
+function argocd_kill_port_forward (){
+	pkill -9 -f "kubectl --context $1 port-forward svc/argocd-server -n argocd $2:80"
+}
+
 function argocd_credentials (){
-    pkill -9 -f "kubectl --context $1 port-forward svc/argocd-server -n argocd $2:80"
+    # This might not need it during workshop
+	# argocd_kill_port_forward $1 $2
     kubectl --context $1 port-forward svc/argocd-server -n argocd $2:80 >/dev/null 2>&1 &
+    # wait for port-forward to be up
+    sleep 3
 	export ARGOCD_PWD=$(kubectl get secrets argocd-initial-admin-secret -n argocd --template='{{index .data.password | base64decode}}' --context $1)
     argocd login "localhost:$2" --plaintext --username admin --password $ARGOCD_PWD --name $1
 	echo "ArgoCD Username: admin"
 	echo "ArgoCD Password: $ARGOCD_PWD"
 	echo "ArgoCD URL: $IDE_URL/proxy/$2"
 }
-
 
 function argocd_hub_credentials (){
 	argocd_credentials fleet-hub-cluster 8081
@@ -18,4 +24,7 @@ function argocd_staging_credentials (){
 function argocd_prod_credentials (){
 	argocd_credentials fleet-prod-cluster 8083
 }
+
+
+
 
