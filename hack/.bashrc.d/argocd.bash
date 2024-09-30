fae31a7cb18679e8d420c852ed8a85c5ea8e03d4
Carlos Santana <carrlos@amazon.com>
2024-09-30 12:08:49 -0400

    kill port-forward to avoid hangs
    Signed-off-by: Carlos Santana <carrlos@amazon.com>

--- hack/.bashrc.d/argocd.bash
+++ hack/.bashrc.d/argocd.bash
@@ -1,12 +1,14 @@
 function argocd_credentials (){
-	kubectl --context $1 port-forward svc/argocd-server -n argocd $2:80 > /dev/null 2>&1 &
+    pkill -9 -f "kubectl --context $1 port-forward svc/argocd-server -n argocd $2:80"
+    kubectl --context $1 port-forward svc/argocd-server -n argocd $2:80 >/dev/null 2>&1 &
 	export ARGOCD_SERVER=$(kubectl get svc -n argocd argocd-server -o jsonpath='{.status.loadBalancer.ingress[0].hostname}' --context $1)
 	export ARGOCD_PWD=$(kubectl get secrets argocd-initial-admin-secret -n argocd --template='{{index .data.password | base64decode}}' --context $1)
-	argocd login "localhost:$2" --plaintext --username admin --password $ARGOCD_PWD --name $1
+    argocd login "localhost:$2" --plaintext --username admin --password $ARGOCD_PWD --name $1
 	echo "ArgoCD Username: admin"
 	echo "ArgoCD Password: $ARGOCD_PWD"
 	echo "ArgoCD URL: $IDE_URL/proxy/$2"
 }
+
 
 function argocd_hub_credentials (){
 	argocd_credentials fleet-hub-cluster 8081
