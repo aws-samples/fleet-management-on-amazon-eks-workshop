f03d9495573cef594e4e1c0eaf2b3b7064d9926f
Workshop User <workshopuser@example.com>
2024-10-23 16:21:09 +0000

    adot

--- terraform/hub/observability.tf
+++ terraform/hub/observability.tf
@@ -85,37 +85,7 @@ resource "aws_sns_topic_subscription" "fleet_alerts_sqs_target" {
 ################################################################################
 # ADOT
 ################################################################################
-#data "aws_partition" "current" {}
 
-# locals{
-#   context = {
-#     aws_caller_identity_account_id = data.aws_caller_identity.current.account_id
-#     aws_caller_identity_arn        = data.aws_caller_identity.current.arn
-#     aws_eks_cluster_endpoint       = module.eks.cluster_endpoint
-#     aws_partition_id               = data.aws_partition.current.partition
-#     aws_region_name                = data.aws_region.current.name
-#     eks_cluster_id                 = module.eks.cluster_name
-#     eks_oidc_issuer_url            = module.eks.oidc_provider
-#     eks_oidc_provider_arn          = module.eks.oidc_provider_arn
-#     tags                           = local.tags
-#     irsa_iam_role_path             = "/"
-#     irsa_iam_permissions_boundary  = null
-#   }  
-
-# }
-
-# module "operator" {
-#   source = "../modules/adot-operator"
-#   enable_cert_manager = true
-#   kubernetes_version  = module.eks.cluster_version
-#   addon_context       = local.context
-# }
-
-# module "collector" {
-#   source = "../modules/adot-collector"
-#   cluster_name = module.eks.cluster_name
-#   tags = local.tags
-# }
 locals{
   adot_collector_namespace = "adot-collector-kubeprometheus"
   adot_collector_serviceaccount = "adot-collector-kubeprometheus"   
