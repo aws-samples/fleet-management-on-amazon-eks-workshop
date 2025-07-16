c8c9c88b8bef2f5e812e650f4df21a8e66a99b3c
Workshop User <workshopuser@example.com>
2025-07-16 09:41:34 +0000

    add fix terraform version
    Signed-off-by: Workshop User <workshopuser@example.com>

--- terraform/spokes/versions.tf
+++ terraform/spokes/versions.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.67.0"
+      version = ">= 4.67.0, < 6.0.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.10.1"
+      version = ">= 2.10.1, < 3.0.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.22.0"
+      version = "2.22.0, < 3.0.0"
     }
   }
 
